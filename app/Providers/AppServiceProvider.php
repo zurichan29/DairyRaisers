7074b52acff7aa76667d3dbb5e02660a99004a2a
shaniawu12 <shaniawu00@gmail.com>
2023-07-19 21:22:21 +0800

    app

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(Request $request): void
     {
+        Schema::disableForeignKeyConstraints();
 
         //
         //Model::unguard();
